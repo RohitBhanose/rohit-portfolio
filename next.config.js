4b8ef9de94579a5243f1dbfb6a7af40d9ab686d8
Rohit Bhanose <rohit.bhanose@gmail.com>
2025-12-07 20:33:59 +0530

    Use Next 14 static export (output: 'export') and add wrangler config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Produce static export (./out) on `next build`
+  output: 'export',
+
   reactStrictMode: true,
+
   images: {
     domains: ['images.unsplash.com'],
   },
 }
 
 module.exports = nextConfig
-
-
-
